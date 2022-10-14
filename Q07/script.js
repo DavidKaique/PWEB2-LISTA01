1972258a9584d4518555a5a4511d5713c10b1ba1
David Kaíque <115568812+DavidKaique@users.noreply.github.com>
2022-10-14 01:05:44 -0300

    Delete Q07 directory

--- Q07/script.js
+++
@@ -1,7 +0,0 @@
-let base = parseInt(prompt("Digite a base: "));
-let altura = parseInt(prompt("Digite a altura: "));
-
-area = base * altura;
-perimetro = 2*(base+altura)
-
-document.write("Area: ",area," ","Perimetro: ", perimetro);
